505a9dc927ee4e0734681f63f63d43d4aa0ae4da
Herli Khoury <92793769+HerliKhoury@users.noreply.github.com>
2023-07-10 13:36:02 -0300

    feat: reviews feats

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import generics
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from reviews.models import Review
+from movies.models import Movie
+from reviews.serializers import ReviewSerializer
+from reviews.permissions import IsAllowedUserOrReadOnly
 
-# Create your views here.
+
+class ReviewView(generics.ListCreateAPIView):
+
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAllowedUserOrReadOnly]
+
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    lookup_url_kwarg = "movie_id"
+
+    def perform_create(self, serializer) -> None:
+        movie_data = get_object_or_404(Movie, id=self.kwargs.get(self.lookup_url_kwarg))
+        serializer.save(critic=self.request.user, movie=movie_data)
